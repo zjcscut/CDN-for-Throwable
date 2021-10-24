7e818c7a1bd60b2533d7f08c47697ff5f1c04fef
throwable <15118850470@153.com>
2021-10-24 11:37:33 +0800

    'swiper'

--- js/swiper_init.js
+++ js/swiper_init.js
@@ -1,22 +1,21 @@
-var swiper = new Swiper('.blog-slider', {
-    passiveListeners: true,
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    autoplay: {
-        disableOnInteraction: true,
-        delay: 3000
-    },
-    mousewheel: true,
-    // autoHeight: true,
-    pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-    }
-});
-
 var comtainer = document.getElementById('swiper_container');
 if (comtainer) {
+    var swiper = new Swiper('.blog-slider', {
+        passiveListeners: true,
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        autoplay: {
+            disableOnInteraction: true,
+            delay: 3000
+        },
+        mousewheel: true,
+        // autoHeight: true,
+        pagination: {
+            el: '.blog-slider__pagination',
+            clickable: true,
+        }
+    });
     comtainer.onmouseenter = function() {
         swiper.autoplay.stop();
     };
